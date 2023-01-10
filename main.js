9e56db655f7658565f2ae8b936e1b7947792e715
Sidyas <slobodadominik@gmail.com>
2023-01-10 07:23:45 +0100

    finalizing exercise

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-import './style.css'
+import './style.css';
+// Zvislá čiara a obľúbený učiteľ
 
+// KONIEC Zvislá čiara a obľúbený učiteľ
+
+zvislaCiara.addEventListener('click', () => {
+    if (!zvislaCiara.classList.contains('animuj')) {
+        if (zvislaCiara.classList.contains('animuj-spat')) {
+            zvislaCiara.classList.remove('animuj-spat');
+            oblubenyUcitel.classList.remove('animuj-ucitela-spat');
+        }
+        zvislaCiara.classList.add('animuj');
+        oblubenyUcitel.classList.add('animuj-ucitela');
+    } else {
+        zvislaCiara.classList.remove('animuj')
+        zvislaCiara.classList.add('animuj-spat');
+        oblubenyUcitel.classList.remove('animuj-ucitela');
+        oblubenyUcitel.classList.add('animuj-ucitela-spat');
+    }
+})
